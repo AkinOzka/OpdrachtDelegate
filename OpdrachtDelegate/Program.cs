e1819d92651aa9c5d76579aa7af52d16a036d1e9
AkinOzka <akinozka@gmail.com>
2022-03-14 00:03:13 +0100

    Add project files.

---
+++ OpdrachtDelegate/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpdrachtDelegate
+{
+    
+    internal class Program
+    {
+        public delegate int Operation(int x, int y);
+        static void Main(string[] args)
+        {
+            SimpleMath simpleMath = new SimpleMath();
+            Operation operation = new Operation(simpleMath.Add);
+            int result=operation(10, 2);
+            Console.WriteLine(result);
+
+            Operation operation2 = new Operation(simpleMath.Subtract);
+            int result2=operation2(10, 2);
+            Console.WriteLine(result2);
+
+            Operation operation3 = new Operation(simpleMath.Multiply);
+            int result3 = operation3(10, 2);
+            Console.WriteLine(result3);
+
+            Operation operation4 = new Operation(simpleMath.Divide);          
+            Console.WriteLine(operation4.Invoke(10,2));
+        }
+    }
+}
---
+++ OpdrachtDelegate/SimpleMath.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpdrachtDelegate
+{   
+    public class SimpleMath
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+        public int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+
+        public int Divide(int x, int y)
+        {
+            return x / y;
+        }
+    }
+}
